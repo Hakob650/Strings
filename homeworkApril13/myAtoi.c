38b985ccafe3b5771d6f92f63d023f1c50e3e3b0
Hakob650 <hakob11meneshian@gmail.com>
2025-04-15 00:04:20 +0400

    Add files via upload

---
+++ homeworkApril13/myAtoi.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int atoi(const char *str){
+	int result=0;
+	int sign=1;
+	int index=0;
+	if(str[index]=='-'){
+		sign=-1;
+		index++;
+	}for(;str[index]!='\0';++index){
+	     if(str[index]>='0' && str[index]<='9'){
+		result=result*10+(str[index]-'0');
+	} 
+        }return sign*result;
+}
+int main(){
+	char str[100];
+	printf("enter string");
+	fgets(str, sizeof(str), stdin);
+	char *p = str;
+    while (*p){
+        if(*p=='\n'){
+            *p='\0';
+            break;
+        }p++;
+    }
+	int value = atoi(str);
+        printf("Integer value: %d\n", value);
+	return 0;
+}
