4fba1c1bec101704db21a4e483c36745014e216c
Hakob650 <hakob11meneshian@gmail.com>
2025-04-23 12:31:08 +0400

    Update task2.c

--- April11/task2.c
+++ April11/task2.c
@@ -1,1 +1,23 @@
-
+#include <stdio.h>
+#include <string.h>
+char* myStrcpy(char* dest, const char* src) {
+    char*original=dest;
+     while(*src!='\0'){
+	     *dest=*src;
+	      dest++;
+              src++;
+    }*dest='\0';
+     return original;
+}
+int main(){
+	char src[100];
+        char dest[100];
+        printf("enter source ");
+        fgets(src, sizeof(src), stdin);
+	int len=strlen(src);
+        if(len>0&&src[len-1]=='\n'){
+        src[len-1]='\0';
+        myStrcpy(dest, src);
+        printf("copied string \"%s\"\n", dest);
+        }return 0;
+}
